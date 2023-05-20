1b6c2f6d11527e7f063b494c970f239f72d024b5
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-05-19 20:38:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Pioreactor high-temp plugin
+-----------------------------
+
+> **Warning**
+> Do not use this. Using this risks damaging your Pioreactor or harming yourself. All responsibility is on you.
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pioreactor_high_temp_plugin",
+    version="0.0.1",
+    license="MIT",
+    description="Do not use this. Using this risks damaging your Pioreactor or harming yourself. All responsibility is on you.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="cam@pioreactor.com",
+    author="Cam Davidson Pilon",
+    url="https://github.com/Pioreactor/pioreactor-high-temp-plugin",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "pioreactor.plugins": "pioreactor_high_temp_plugin = pioreactor_high_temp_plugin"
+    },
+)
