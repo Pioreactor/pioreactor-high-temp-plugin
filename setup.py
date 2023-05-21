978a97f49963f3f215674c68d6bc6f8384149eec
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-05-21 12:09:46 -0400

    update script with new time constants

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pioreactor_high_temp_plugin",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     description="Do not use this. Using this risks damaging your Pioreactor or harming yourself. All responsibility is on you.",
     long_description=open("README.md").read(),
