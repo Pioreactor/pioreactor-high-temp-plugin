9853be90412bab70ad6d1e095cff402a63759df5
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-10-04 18:39:36 -0400

    new author

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pioreactor-high-temp-plugin",
-    version="0.1.0",
+    version="0.1.1",
     license="MIT",
     description="Do not use this. Using this risks damaging your Pioreactor or harming yourself. All responsibility is on you.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author_email="cam@pioreactor.com",
-    author="Cam Davidson Pilon",
+    author="Pioreactor",
     url="https://github.com/Pioreactor/pioreactor-high-temp-plugin",
     packages=find_packages(),
     include_package_data=True,
